c2897bcc05c472138e3daa97035f1e84c6f33b5f
Raul Pegan <raulpegan1@gmail.com>
2017-07-13 18:52:14 -0700

    Startup scripts

---
+++ .config/i3/start.sh
@@ -0,0 +1,14 @@
+i3-msg 'workspace main'
+i3-msg exec "urxvt -name urxvtscratch"
+i3-msg exec "chromium"
+sleep 3
+i3-msg 'workspace term'
+i3-msg exec "urxvt -e tmux"
+sleep 1
+i3-msg 'workspace chat'
+i3-msg exec "slack"
+sleep 5
+i3-msg 'workspace evrn'
+i3-msg exec "whatever"
+i3-msg '[instance="urxvtscratch"] move scratchpad'
+i3-msg 'workspace main'
