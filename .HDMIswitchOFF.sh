3380df481fb42d3f4112d268ef83430ebbd13acc
Raul Pegan <raulpegan1@gmail.com>
2017-10-25 19:15:10 -0700

    Update/fix

--- .HDMIswitchOFF.sh
+++ .HDMIswitchOFF.sh
@@ -2,5 +2,6 @@
 
 ~/.fehbg
 ~/.screenlayout/no.sh
-xinput set-prop "Synaptics TM3073-003" "libinput Natural Scrolling Enabled" 1
+xinput set-prop "Synaptics TM3072-003" "libinput Natural Scrolling Enabled" 1
+xinput set-prop "Synaptics TM3072-003" "libinput Tapping Enabled" 1
 sed -i -e 's/monitor =.*$/monitor = eDP1/g' ~/.config/polybar/config
--- .HDMIswitchStartup.sh
+++ .HDMIswitchStartup.sh
@@ -5,6 +5,7 @@ extern=HDMI1
 if xrandr | grep "$extern disconnected"; then
     ~/.screenlayout/no.sh
     xinput set-prop "Synaptics TM3072-003" "libinput Natural Scrolling Enabled" 1
+    xinput set-prop "Synaptics TM3072-003" "libinput Tapping Enabled" 1
     sed -i -e 's/monitor =.*$/monitor = eDP1/g' ~/.config/polybar/config
 else
     ~/.screenlayout/office.sh
